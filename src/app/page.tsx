28033ee9a6f81a4fcd78718c58f7a679364e2a8f
MahloKgotleng <109388716+MahloKgotleng@users.noreply.github.com>
2026-04-18 10:13:15 +0200

    Add main page layout with multiple sections

---
+++ src/app/page.tsx
@@ -0,0 +1,29 @@
+import Navigation from '@/components/fragments/Navigation'
+import HeroSection from '@/components/fragments/HeroSection'
+import SolutionsSection from '@/components/fragments/SolutionsSection'
+import IndustriesSection from '@/components/fragments/IndustriesSection'
+import ProcessSection from '@/components/fragments/ProcessSection'
+import AboutSection from '@/components/fragments/AboutSection'
+import ResultsSection from '@/components/fragments/ResultsSection'
+import PartnersSection from '@/components/fragments/PartnersSection'
+import ContactSection from '@/components/fragments/ContactSection'
+import Footer from '@/components/fragments/Footer'
+import ChatBot from '@/components/fragments/ChatBot'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <Navigation />
+      <HeroSection />
+      <SolutionsSection />
+      <IndustriesSection />
+      <ProcessSection />
+      <AboutSection />
+      <ResultsSection />
+      <PartnersSection />
+      <ContactSection />
+      <Footer />
+      <ChatBot />
+    </main>
+  )
+}
